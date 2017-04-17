566a63060f70a1d2aa56191e055ff44e61cb62b3
necrodiver <neclodiver@live.com>
2017-04-17 16:19:09 +0800

    添加内容

---
+++ AddMIME/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddMIME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Begin();
+        }
+
+        public static void AddMIMEType()
+        {
+            DirectoryEntry rootEntry = GetDirectoryEntry("IIS://localhost/w3svc/1/root");
+            foreach (PropertyValueCollection pc in rootEntry.Properties)
+            {
+                Console.WriteLine(pc.PropertyName + ":" + pc.Value);
+            }
+            //rootEntry.Properties["MimeMap"].Add(
+            IISOle.MimeMapClass _NewMime = new IISOle.MimeMapClass();          //新建MIME类型
+            _NewMime.Extension = ".xap";
+            _NewMime.MimeType = ".xap";
+            rootEntry.Properties["MimeMap"].Add(_NewMime);  //添加MIME类型
+
+            rootEntry.CommitChanges();//更改目录
+
+        }
+    }
+
+}
